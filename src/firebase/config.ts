bf4e791c4cb7d05a61e67f9402504be1765371b1
Krzysztof Sobczak <abaknet.krzysztof@gmail.com>
2026-03-22 20:48:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4075507772-6bfaa",
+  "appId": "1:222895374787:web:860a1fc03bfbadfe0e3d76",
+  "apiKey": "AIzaSyC8aM_rGMlOd6lDZLV8TCGzPgeIwhQF6dw",
+  "authDomain": "studio-4075507772-6bfaa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "222895374787"
+};
